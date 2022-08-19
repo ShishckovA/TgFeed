29d74e76eb927e30f7330db037b36097d1f753fe
Alexey Shishkov <33600436+ShishckovA@users.noreply.github.com>
2022-08-20 00:35:38 +0300

    Refactor, add more accepted urls

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telethon.tl.types import Channel, Message
 from config import logger
 
 from config import API_ID, API_HASH, ACCEPTED_CHANNELS, TARGET_CHANNEL, \
-    TRASH_CHANNEL
+    TRASH_CHANNEL, ACCEPTED_URLS
 
 client = TelegramClient('tg_feed', API_ID, API_HASH)
 target_channel: Optional[Channel] = None
@@ -34,19 +34,35 @@ def is_ad_tag(tag: Union[
     logger.info(f"Processing tag {tag.to_dict()} "
                  f"({message.message[tag.offset:tag.offset + tag.length]}, "
                  f"type {type(tag)}")
-    is_ad = type(tag) in [
+    is_mention_tag = type(tag) in [
         types.MessageEntityCashtag,
         types.MessageEntityBankCard,
         types.MessageEntityEmail,
         types.MessageEntityMention,
         types.MessageEntityMentionName,
         types.MessageEntityUnknown,
-    ] or (type(tag) == types.MessageEntityTextUrl and
-          'teletype' not in tag.url) \
-            or (type(tag) == types.MessageEntityUrl and
-                'teletype' not in message.message[
-                                  tag.offset:tag.offset + tag.length])
-    logger.info(f"Is {'' if is_ad else 'not '}an ad tag")
+    ]
+    logger.info(f"Is {'' if is_mention_tag else 'not '}a mention tag")
+    is_url_tag = type(tag) in [
+        types.MessageEntityTextUrl, types.MessageEntityUrl
+    ]
+    is_bad_url_tag = True
+    if is_url_tag:
+        url_part = (
+            tag.url if type(tag) == types.MessageEntityTextUrl 
+            else message.message[tag.offset:tag.offset + tag.length]
+        )
+        logger.info(f"Checking if any of {ACCEPTED_URLS} is in {url_part}")
+        for good_url in ACCEPTED_URLS:
+            if good_url in url_part:
+                is_bad_url_tag = False
+                logger.info(f"{good_url} is in url_part, tag is ok")
+                break
+    is_ad = is_mention_tag or (is_url_tag and is_bad_url_tag)
+    logger.info(
+        f"Mention tag: {is_mention_tag}, url tag: {is_url_tag}, "
+        f"bad url tag: {is_bad_url_tag}"
+    )
     return is_ad
 
 
